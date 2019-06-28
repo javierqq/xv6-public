c699b3ba93011b934c1f0aad9e00cb087f1a2633
Javier Quiroz <javierqq@gmail.com>
2019-06-28 10:21:29 -0400

    reverted to previous

--- proc.c
+++ proc.c
@@ -538,12 +538,11 @@ int translate(void)
   struct proc *process = myproc();   //accedo al proceso actual
    int pgtab;
    if(PTE_P){
-       pgtab = (int)P2V(PTE_ADDR(process->pgdir))  //P2V() suma 0x80000000 y PTE tiene la dirección física
+       pgtab = (int)P2V(PTE_ADDR(process->pgdir));  //P2V() suma 0x80000000 y PTE tiene la dirección física
 
        return pgtab;
    }
    else{
        return 0;
    }
-
 }
